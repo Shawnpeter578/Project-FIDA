8e22cddd7a936ce716ec389214207c22deb81c6c
XPZ4Y <xanderquasar@gmail.com>
2026-02-18 23:34:19 +0530

    CORS changes to apply vercel app

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ const apiLimiter = rateLimit({
 });
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://independent-irita-clubspot-9e43f2fa.koyeb.app'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://independent-irita-clubspot-9e43f2fa.koyeb.app', 'https://gig-by-city.vercel.app'],
   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
 }));
 
